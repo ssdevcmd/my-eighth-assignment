dfa31dce52a9d6ce83a141d27aaeb8032ae6f662
ssdevcmd <ssph079@gmail.com>
2026-05-17 15:50:14 +0600

    changes in the footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -39,13 +39,13 @@ const Footer = () => {
                         </h3>
 
                         <div className="flex flex-col gap-2 text-sm">
-                            <Link href="/terms">
+                            <p>
                                 Terms & Conditions
-                            </Link>
+                            </p>
 
-                            <Link href="/privacy">
+                            <p>
                                 Privacy Policy
-                            </Link>
+                            </p>
                         </div>
                     </div>
                 </div>
