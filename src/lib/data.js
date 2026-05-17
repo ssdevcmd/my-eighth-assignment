f8e158a41db92d8ad0271ff72f678f7ba6528651
ssdevcmd <ssph079@gmail.com>
2026-05-17 11:20:16 +0600

    some changes to fetch data

--- src/lib/data.js
+++ src/lib/data.js
@@ -3,3 +3,9 @@ import courses from "../../public/courses.json";
 export async function getCourses() {
   return courses;
 }
+
+export function getCourseById(id) {
+  return courses.find(
+    (course) => course.id.toString() === id.toString()
+  );
+}
